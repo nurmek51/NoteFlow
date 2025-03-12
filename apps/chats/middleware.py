811f816fdaa04d061b533b7f7255f83f731cacb1
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-12 19:01:35 +0500

    Added custom middleware. Extract and token validation

---
+++ apps/chats/middleware.py
@@ -0,0 +1,39 @@
+import urllib.parse
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+@database_sync_to_async
+def get_user_from_token(token):
+    jwt_auth = JWTAuthentication()
+    try:
+        validated_token = jwt_auth.get_validated_token(token)
+        user = jwt_auth.get_user(validated_token)
+        return user
+    except Exception as e:
+        print("JWT token validation error:", e)
+        return AnonymousUser()
+
+class JWTAuthMiddleware:
+    """
+    Middleware for auth through JWT, which extract the token from query string and put the user in the scope
+    """
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get("query_string", b"").decode()
+        query_params = urllib.parse.parse_qs(query_string)
+        token_list = query_params.get("token", None)
+        token = token_list[0] if token_list else None
+
+        if token:
+            scope["user"] = await get_user_from_token(token)
+        else:
+            scope["user"] = AnonymousUser()
+
+        print("User in scope:", scope["user"])
+        return await self.inner(scope, receive, send)
+
+def JWTAuthMiddlewareStack(inner):
+    return JWTAuthMiddleware(inner)
