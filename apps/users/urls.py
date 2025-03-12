83ee44fd0f27299906a997a772fba90077590430
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-12 10:35:55 +0500

    Added user auth

---
+++ apps/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserRegistrationView
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserRegistrationView.as_view(), name='register_user')
+]
+
+# {
+#     "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc0MjI3NDUzNywiaWF0IjoxNzQxNjY5NzM3LCJqdGkiOiJjNDFlYTg1ZjllZDA0Zjg3YWI2ZjI3ZTcwYjJlNmEzNyIsInVzZXJfaWQiOjF9.jNyBrEvWKchxY57y-JM6eNkJVhuNPilmbNVfo45tnt4",
+#     "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQxNzU2MTM3LCJpYXQiOjE3NDE2Njk3MzcsImp0aSI6IjA2NDBlZmY0NDhkZTRhOWM5YzU4NDBjNTE1MDQwMzM0IiwidXNlcl9pZCI6MX0.fAqRJOk1g2bBA_zQUjUL-Cv0j4RjSbDO67YJwW5LBMM"
+# }
---
+++ apps/users/views.py
@@ -0,0 +1,31 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserRegistrationSerializer
+
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    """
+    API endpoint for user registration.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        """
+        Handles user registration by validating and creating a new user.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            {
+                "message": "User registered successfully",
+                "username": user.username
+            },
+            status=status.HTTP_201_CREATED
+        )
