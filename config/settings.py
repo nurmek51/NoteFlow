7f4888d44141e45a8f8997177f18bcba770d12e7
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-12 22:48:12 +0500

    Configured S3 storage

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+# Config S3 storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Settings S3
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-north-1')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,6 +27,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 INSTALLED_APPS = [
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
