bb8a71a741e21ac682ab626046853862da243b47
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-13 16:48:31 +0500

    Added JPEG file format

--- apps/materials/models.py
+++ apps/materials/models.py
@@ -10,7 +10,6 @@ class Subject(models.Model):
 
 class Lecturer(models.Model):
     name = models.CharField(max_length=255)
-    university = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name
