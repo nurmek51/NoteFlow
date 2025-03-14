65a59a8552015284dabb394eac0653fbcd150bd3
sup1p <sup4anski@gmail.com>
2025-03-14 20:56:07 +0500

    added forgot-password function, EMAIL_BACKEND - EMAIL_HOST - EMAIL_PORT - EMAIL_USE_TLS - EMAIL_HOST_USER - EMAIL_HOST_PASSWORD - DEFAULT_FROM_EMAIL -> needs to be filled in env

--- config/settings.py
+++ config/settings.py
@@ -177,3 +177,12 @@ LOGGING = {
     },
 }
 
+# Настройки Email для продакшена
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.users.urls')),  # Подключаем users.urls
-    path('api/', include('apps.materials.urls')) # Включил материалы корочежиесть
+    path('api/', include('apps.materials.urls')), # Включил материалы корочежиесть
+
+
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
