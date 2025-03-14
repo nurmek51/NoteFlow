f27448caeee60daedd3f1a2a8bc48f8824435ced
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-14 12:30:34 +0500

    Edited DB settings

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,10 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
+
 
 
 # Config S3 storage
@@ -101,14 +103,7 @@ ASGI_APPLICATION = 'config.asgi.application'
 
 # Database settings (PostgreSQL)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
