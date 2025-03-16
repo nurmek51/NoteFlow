08c13438a4528cca330bc0d889bf08903166ab14
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-17 01:01:43 +0500

    Fixed routes, now user must access the group id to connect

--- apps/chats/routing.py
+++ apps/chats/routing.py
@@ -2,8 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Public chat
-    re_path(r'^ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    # Private chat
-    re_path(r'^ws/private/(?P<room_name>[\w-]+)/$', consumers.PrivateChatConsumer.as_asgi()),
+    re_path(r'^ws/group/(?P<group_id>\d+)/$', consumers.GroupChatConsumer.as_asgi()),
 ]
