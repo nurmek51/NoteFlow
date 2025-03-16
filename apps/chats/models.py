8010a10f24e1176b1daa9957e6f2749dc0cf3032
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-17 01:00:16 +0500

    Added new parameters to ChatMessage model

--- apps/chats/models.py
+++ apps/chats/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from ..users.models import User
+from ..users.models import User, StudyGroup
 
 class ChatMessage(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages', null=True, blank=True)
+    group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE, related_name='group_messages', null=True, blank=True)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Message from {self.sender.username} to {self.receiver.username}'
+        if self.group:
+            return f'Group Message in {self.group.name} from {self.sender.username}'
+        return f'Private Message from {self.sender.username} to {self.receiver.username}'
