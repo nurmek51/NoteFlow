f8b762139894050dcc5e9457446083e48c531892
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-19 16:55:07 +0500

    Configured settings for Swagger

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',  # JWT Authentication
     'channels',
+    'drf_spectacular',
     'corsheaders',
 
     # Custom apps
@@ -127,6 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # JWT Authentication settings
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -135,6 +137,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
 }
 
 # JWT configuration
